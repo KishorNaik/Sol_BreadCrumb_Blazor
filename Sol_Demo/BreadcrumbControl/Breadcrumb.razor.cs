5f42c450573f44d16b3aff8014c9ba3ed4f06549
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-22 17:44:25 +0530

    Code Commit

---
+++ Sol_Demo/BreadcrumbControl/Breadcrumb.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadcrumbControl
+{
+    public partial class Breadcrumb
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
---
+++ Sol_Demo/BreadcrumbControl/BreadcrumbNavLink.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadcrumbControl
+{
+    public partial class BreadcrumbNavLink
+    {
+        [Parameter]
+        public String Name { get; set; }
+
+        [Parameter]
+        public String Href { get; set; }
+
+        [Parameter]
+        public bool IsActive { get; set; }
+    }
+}
